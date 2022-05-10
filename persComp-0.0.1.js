3704bd27f9a4a93dec1bb846f46b3e3e948d8e73
Christian Rojas <44682470+BlueFits@users.noreply.github.com>
2022-05-09 22:50:41 -0400

    Init commit

---
+++ persComp-0.0.1.js
@@ -0,0 +1,31 @@
+;((function(global) {
+    
+    // 'new' an object
+    const PersComponent = function() {
+        return new PersComponent.init();   
+    }
+
+    // prototype holds methods (to save memory space)
+    PersComponent.prototype = {
+        // 'this' refers to the calling object at execution time
+        HintBox: function(h1) {
+            return '<div><h1>' + h1 + '</h1></div>'; 
+        },
+    
+    };
+    
+    // the actual object is created here, allowing us to 'new' an object without calling 'new'
+    PersComponent.init = function() {
+        const self = this;
+        // self.firstName = firstName || '';
+        // self.lastName = lastName || '';
+        // self.language = language || 'en';
+                
+    }
+    
+    // trick borrowed from jQuery so we don't have to use the 'new' keyword
+    PersComponent.init.prototype = PersComponent.prototype;
+    
+    global.PersComponent = global.$P =  PersComponent;
+    
+})(window));
